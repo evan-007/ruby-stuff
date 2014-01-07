e00ffba6d994c09b857395fc4adf99582c35687f
Evan Lloyd <eulloyd@gmail.com>
2014-01-07 14:06:34 +0900

    Adds fizzbuzz

--- fizz-buzz.rb
+++ fizz-buzz.rb
@@ -1,8 +1,10 @@
 for num in 1...100
-	if num % 3 == 0
-		puts "fizz"
+	if num % 3 == 0 && num % 5 == 0
+		puts "Fizz Buzz"
+	elsif num % 3 == 0
+		puts "Fizz"
 	elsif num % 5 == 0
-		puts "buzz"
+		puts "Buzz"
 	else
 		puts num
 	end
