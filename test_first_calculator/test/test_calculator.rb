b76375c40663f7ed36f2b07c6adf9be22f3a0e9f
Evan Lloyd <eulloyd@gmail.com>
2014-01-09 11:23:23 +0900

    DRYs tests

--- test_first_calculator/test/test_calculator.rb
+++ test_first_calculator/test/test_calculator.rb
@@ -2,21 +2,24 @@ require 'test/unit'
 require 'app/calculator'
 
 class CalculatorTest < Test::Unit::TestCase
+
+	def setup
+		@calculator = Calculator.new
+	end
+
+
 	def test_can_add
-		calculator = Calculator.new
-		actual = calculator.add(2, 2)
+		actual = @calculator.add(2, 2)
 		assert_equal(4, actual)
 	end
 
 	def test_can_subtract
-		calculator = Calculator.new
-		actual = calculator.subtract(5, 3)
+		actual = @calculator.subtract(5, 3)
 		assert_equal(2, actual)
 	end
 
 	def test_can_multiply
-		calculator = Calculator.new
-		actual = calculator.multiply(6, 3)
+		actual = @calculator.multiply(6, 3)
 		assert_equal(18, actual)
 	end
 end
